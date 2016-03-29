00010d5719971b2e7c5c2467ac46846f00686db2
marcomanomat <marco.manomat@gmail.com>
2016-03-28 21:57:56 -0400

    add correct string methods to example objects

--- ruby/method_detective.rb
+++ ruby/method_detective.rb
@@ -4,36 +4,36 @@
 # When there's more than one suspect who could have
 # committed the crime, add additional calls to prove it.
 
-# “iNvEsTiGaTiOn”.<???>
-# => “InVeStIgAtIoN”
+p "iNvEsTiGaTiOn".swapcase
+# => "InVeStIgAtIoN"
 
-# “zom”.<???>
-# => “zoom”
+ p "zom".insert(2,"o")
+# => "zoom"
 
-# “enhance”.<???>
-#=> “    enhance    ”
+p "enhance".strip
+#=> "    enhance    "
 
-# “Stop! You’re under arrest!”.<???>
-# => “STOP! YOU’RE UNDER ARREST!”
+p "Stop! You’re under arrest!".upcase
+# => "STOP! YOU’RE UNDER ARREST!"
 
-# “the usual”.<???>
-#=> “the usual suspects”
+p "the usual".<<" suspects"
+#=> "the usual suspects"
 
-#“ suspects”.<???>
-#=> “the usual suspects”
+p " suspects".prepend("the usual")
+#=> "the usual suspects"
 
-# “The case of the disappearing last letter”.<???>
-# => “The case of the disappearing last lette”
+p "The case of the disappearing last letter".chop
+# => "The case of the disappearing last lette"
 
-# “The mystery of the missing first letter”.<???>
-# => “he mystery of the missing first letter”
+p "The mystery of the missing first letter".tr('T',"")
+# => "he mystery of the missing first letter"
 
-# “Elementary  ,    my   dear      Watson!”.<???>""
-# => “Elementary, my dear Watson!”
+p "Elementary,    my   dear      Watson!".squeeze(" ")
+# => "Elementary, my dear Watson!"
 
-#“z”.<???>
+p "z".ord
 # => 122 
 # (What is the significance of the number 122 in relation to the character z?)
 
-#“How many times does the letter ‘a’ appear in this string?”.<???>
+p "How many times does the letter ‘a’ appear in this string?".count "a"
 #=> 4
