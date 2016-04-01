cddee0b4768a3efeafcfc2e45d6a84ad17b1e252
marcomanomat <marco.manomat@gmail.com>
2016-03-31 21:50:31 -0400

    redo this

--- ruby/method_detective.rb
+++ ruby/method_detective.rb
@@ -4,6 +4,7 @@
 # When there's more than one suspect who could have
 # committed the crime, add additional calls to prove it.
 
+<<<<<<< HEAD
 p "iNvEsTiGaTiOn".swapcase
 # => "InVeStIgAtIoN"
 
@@ -37,3 +38,38 @@ p "z".ord
 
 p "How many times does the letter ‘a’ appear in this string?".count "a"
 #=> 4
+=======
+# "iNvEsTiGaTiOn".<???>
+# => “InVeStIgAtIoN”
+
+# "zom".<???>
+# => “zoom”
+
+# "enhance".<???>
+# => "    enhance    "
+
+# "Stop! You’re under arrest!".<???>
+# => "STOP! YOU’RE UNDER ARREST!"
+
+# "the usual".<???>
+#=> "the usual suspects"
+
+# " suspects".<???>
+# => "the usual suspects"
+
+# "The case of the disappearing last letter".<???>
+# => "The case of the disappearing last lette"
+
+# "The mystery of the missing first letter".<???>
+# => "he mystery of the missing first letter"
+
+# "Elementary,    my   dear        Watson!".<???>
+# => "Elementary, my dear Watson!"
+
+# "z".<???>
+# => 122 
+# (What is the significance of the number 122 in relation to the character z?)
+
+# "How many times does the letter 'a' appear in this string?".<???>
+# => 4
+>>>>>>> upstream/master
