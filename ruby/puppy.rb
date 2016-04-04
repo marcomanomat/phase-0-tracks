2251a66f442bffd055938d87a7203e6a130476c6
marcomanomat <marco.manomat@gmail.com>
2016-04-04 18:19:17 -0400

    add characteristics/behavior attributes to a Puppy class.

---
+++ ruby/puppy.rb
@@ -0,0 +1,8 @@
+#6.1 Introduction to classes
+
+#Release 0: Design a class
+#A Puppy class might have name, color, size, breed, energy-level, friendliness
+
+#Release 1: Delcare your class and create an instance in IRB
+
+#Release 2: Practice using class methods and instance methods (in IRB)
