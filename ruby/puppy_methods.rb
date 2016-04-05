7c40c089a709f4893e20228a1c1976cb16b7c625
marcomanomat <marco.manomat@gmail.com>
2016-04-04 21:02:14 -0400

    finish release 0: add speak, roll-over, dog years methods

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -5,4 +5,22 @@ class Puppy
     toy
   end
 
+  def speak(number)
+  	number.times do puts "Woof!" end
+  end
+
+  def roll_over
+  	puts "*rolls over*"
+  end
+
+  def dog_years(human_years)
+  	 return 11 * human_years
+  end
+
 end
+
+shishi = Puppy.new
+shishi.fetch("squeeky ball")
+shishi.speak(5)
+shishi.roll_over
+shishi.dog_years(7)
