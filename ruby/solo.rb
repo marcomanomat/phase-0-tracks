e18519e8df891ae9f67cbfb3fb4c3c1a8795dfbb
marcomanomat <marco.manomat@gmail.com>
2016-04-06 16:50:05 -0400

    finish release 0: design a basketball player class, add attributes and methods to pseudocode.

--- ruby/solo.rb
+++ ruby/solo.rb
@@ -1,1 +1,13 @@
 #6.4: Solo Challenge: Designing and Implementing a Class
+
+#Release 0: Design a Class
+
+#3 Attributes/Characteristics
+#Height (in inches) - integer
+#Weight (in lbs) - integer
+#Best Basketball Related Quality - string
+
+#3 Methods
+#Shoot(how far?)
+#Dribble(to where?)
+#Pass(to who?)
