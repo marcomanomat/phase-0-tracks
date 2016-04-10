c4c08d8e0161290b0475c4f909be87d7f0d2c8b7
marcomanomat <marco.manomat@gmail.com>
2016-04-10 19:09:37 -0400

    change loop to .times because number of times is given.

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -1,7 +1,8 @@
+
 class Puppy
 
   def initialize
-  	puts "Initializing new puppy instance..."
+    puts "Initializing new puppy instance..."
   end
 
   def fetch(toy)
@@ -10,19 +11,19 @@ class Puppy
   end
 
   def speak(number)
-  	number.times do puts "Woof!" end
+    number.times do puts "Woof!" end
   end
 
   def roll_over
-  	puts "*rolls over*"
+    puts "*rolls over*"
   end
 
   def dog_years(human_years)
-  	 return 11 * human_years
+     return 11 * human_years
   end
 
   def sleep_whole_day
-  	puts "*sleeps whole day* and loves every second of it!"
+    puts "*sleeps whole day* and loves every second of it!"
   end
 
 end
@@ -42,38 +43,37 @@ class Kitten
   end
 
   def ignore
-  	puts "*ignores you with ease*"
+    puts "*ignores you with ease*"
   end
 
   def speaks
-  	puts "meeoow" 
+    puts "meeoow" 
   end
   
 end
 
 #updated correct logic
-fifty = []
-  i = 0
-  while i <= 50
-  fifty.push(Kitten.new)
-  i += 1
-end
-p fifty
+fifty_kittens = []
+#since we know how many times we're going to iterate/loop, we use the .times loop
+50.times { fifty_kittens << (Kitten.new) }
 
-fifty.each { |kitten| 
+fifty_kittens.each do |kitten| 
   kitten.ignore 
   kitten.speaks
-}
+end
 
 =begin
 #wrong/original logic:
 fifty = [many = Kitten.new]*50
-
 fifty.each do many.ignore end
 fifty.each do many.speaks end
 p fifty
-=end
 
-
+#We can also use though: 
+  while i <= 50
+  fifty_kittens << (Kitten.new)
+  i += 1
+  end
+=end  
 
 
