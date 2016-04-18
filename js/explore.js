31c65f5fa37b2be74d93028cabe4e92bd60aefec
marcomanomat <marco.manomat@gmail.com>
2016-04-17 22:24:15 -0400

    add code to reverse string in JS

--- js/explore.js
+++ js/explore.js
@@ -1,1 +1,27 @@
-#7.1
+//7.1
+
+
+//we need to make a reverse('hello') function. we will want to set our 
+//counter to 0
+//have an empty string that will hold the letters
+//Then we will have to add the letters together to form the reversed string
+//
+
+
+function reverse(str) {
+  var reversed = "";
+  var i = str.length;
+  while (i>0){
+  	reversed += str.substring(i - 1, i);
+  	i--;
+  }
+  return reversed;
+
+//////DRIVER//////
+}
+	console.log (reverse("hello"));
+
+
+if (1 == 1) {
+	console.log (reverse("hello world"));
+}
