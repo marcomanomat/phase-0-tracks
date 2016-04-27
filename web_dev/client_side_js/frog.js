167bb2a71267ca58aec99bcd7e53b572b6f41543
marcomanomat <marco.manomat@gmail.com>
2016-04-27 19:34:20 -0400

    finish 9.2

---
+++ web_dev/client_side_js/frog.js
@@ -0,0 +1,21 @@
+
+console.log("Testing")
+
+function addTextShadow(event) {
+	console.log(event);
+event.target.style.textShadow = "2px 2px #008000";
+}
+
+
+var frogMiscText = document.getElementsByTagName('p')
+var frogText = frogMiscText[0]
+frogText.addEventListener("mouseover", addTextShadow);
+	
+// 	event.target.style.textShadow = "2px 2px #008000";
+// setTimeout(addTextShadow() {
+// 	event.target.style.color = "";
+//     }, 500);
+//   }, false);
+  
+
+
