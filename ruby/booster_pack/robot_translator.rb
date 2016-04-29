d5327369f1e4facf8e915abecb9fa8996eeb844f
marcomanomat <marco.manomat@gmail.com>
2016-04-29 17:38:38 -0400

    finish first step of the problem

--- ruby/booster_pack/robot_translator.rb
+++ ruby/booster_pack/robot_translator.rb
@@ -1,6 +1,7 @@
 # ROBOT TRANSLATOR
 
-# IF a letter is capitalized and it's in the first half of the alphabet, it becomes: "bloop".
+# If a letter is capitalized and it's in the first half of the alphabet, it becomes: "bloop".
+# A - M (1st half); N - Z (2nd half)
 
 # Otherwise, if a letter is capitalized or it's the letter "e", it becomes: "buzz".
 
@@ -10,3 +11,21 @@
 
 # "Happy Halloween!" => "bloopbeepbeepbeepbeepboingbloopbeepbeepbeepbeepbeepbuzzbuzzbeepboing"
 
+alpha = "abcdefghijklmnopqrstuvwxyz"
+half_1 = alpha[0..12].split("")
+half_2 = alpha[13..26]
+half_1
+
+word = "Happy Halloween!"
+separated_letter_array = word.split("")
+
+new_array = []
+separated_letter_array.each do |letter|
+	if half_1.include?(letter.downcase) && letter == letter.capitalize
+		new_array << "bloop"
+	 else
+		 new_array << letter
+	end
+end
+
+p new_array
