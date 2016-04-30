c447727728d3f2c141675efbb8999fda098ab06e
marcomanomat <marco.manomat@gmail.com>
2016-04-29 21:57:24 -0400

    finish conditions of robot problem

--- ruby/booster_pack/robot_translator.rb
+++ ruby/booster_pack/robot_translator.rb
@@ -2,31 +2,39 @@
 
 # If a letter is capitalized and it's in the first half of the alphabet, it becomes: "bloop".
 # A - M (1st half); N - Z (2nd half)
-# Hint, watch for capitals! - have to use .downcase/.capitalize when needed
 
 # Otherwise, if a letter is capitalized or it's the letter "e", it becomes: "buzz".
 
 # If it's not a letter at all, it becomes: "boing".
 
-# Otherwise, it becomes: "bleep".
+# Otherwise, it becomes: "beep".
 
 # "Happy Halloween!" => "bloopbeepbeepbeepbeepboingbloopbeepbeepbeepbeepbeepbuzzbuzzbeepboing"
 
 alpha = "abcdefghijklmnopqrstuvwxyz"
-half_1 = alpha[0..12].split("")
+half_1 = alpha[0..12]
 half_2 = alpha[13..26]
-half_1
 
 word = "Happy Halloween!"
 separated_letter_array = word.split("")
 
 new_array = []
 separated_letter_array.each do |letter|
-	if half_1.include?(letter.downcase) && letter == letter.capitalize
+	if 
+		half_1.include?(letter.downcase) && letter == letter.capitalize
 		new_array << "bloop"
-	 else
-		 new_array << letter
+	elsif
+		unless alpha.include?(letter)
+		new_array << "boing"
+		end
+	elsif
+		letter == letter.capitalize || letter == "e"
+		new_array << "buzz"
+	else
+		new_array << "beep"
 	end
 end
 
-p new_array
+robot = new_array.join('')
+
+p robot
